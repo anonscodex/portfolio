fcf21ba2680633972f35557db2bd92647a0e93f1
anonscodex <normalstuff2001@gmail.com>
2023-12-06 19:43:39 +0000

    added animation to page

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
-var typed = new Typed('.element', {
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+let typed = new Typed('.element', {
     strings: ['  ', 'Yusuf Abdullah Ayomide', 'a Frontend Developer', 'a Graphic Designer'],
     smartBackspace: true ,// Default value
     typeSpeed:70,
