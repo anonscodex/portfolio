7fe4803e666dedb099dc06fce6f77c32bc18c506
anonscodex <normalstuff2001@gmail.com>
2023-12-06 10:31:27 +0000

    added tools and project section

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var typed = new Typed('.element', {
+    strings: ['  ', 'Yusuf Abdullah Ayomide', 'a Frontend Developer', 'a Graphic Designer'],
+    smartBackspace: true ,// Default value
+    typeSpeed:70,
+    backSpeed:40,
+    startDelay:700,
+    backDelay: 700,
+    loop: true,
+  loopCount: Infinity
+
+  });
